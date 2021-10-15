3dece6da56278cd79d2dedc40bfa209ab33c4131
kelli-alan <alankelli64@gmail.com>
2021-10-14 21:38:17 -0400

    Set up JSON simple

---
+++ src/DataWriter.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+public class DataWriter extends DataConstants {
+  
+}
