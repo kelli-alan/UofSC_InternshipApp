69e482952c8719f8eb0008864af1d099ede6fa4c
PikHacker <evangrune@gmail.com>
2021-10-18 18:57:34 -0400

    got Resume class mostly done

--- src/Resume.java
+++ src/Resume.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.UUID;
+
 import java.time.Month;
 
 enum Major {
@@ -10,7 +11,7 @@ enum Major {
 
 /**
  * 
- * @authors Robbie Clark
+ * @authors Robbie Clark, Evan Grunewald
  */
 public class Resume {
     
@@ -19,63 +20,92 @@ public class Resume {
     private String lastName;
     private String eMail;
     private String phoneNum;
+    private ArrayList<String> skills;
     private ArrayList<Education> education;
-    //insert work experience
-    //insert extracurricular
+    private ArrayList<WorkExperience> workExperiences;
+    private ArrayList<Extracurricular> extracurriculars;
 
-    //add ID
-    public Resume(String firstName, String lastName) { 
-
+    public Resume(UUID id, String firstName, String lastName) { 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.skills = new ArrayList<String>();
+        this.workExperiences = new ArrayList<WorkExperience>();
+        this.education = new ArrayList<Education>();
+        this.extracurriculars = new ArrayList<Extracurricular>();
     }
 
     public void addWorkExperience(String company, String position, Month startMonth, int startYear, Month endMonth, int endYear) {
-
+        // City and State are missing here?
+        this.workExperiences.add(new WorkExperience(position, startMonth, startYear, company, "city", "state"));
     }
 
     public void addEducation(String university, String city, String state, String degreeType, Major major, String minor, Month gradMonth, int gradYear, double GPA) {
-
+        Education temp = new Education(university, city, degreeType, major, gradMonth, gradYear);
+        temp.addGPA(GPA);
+        temp.addMinor(minor);
+        this.education.add(temp);
     }
-
     
     public void addSkill(String skill) {
-
+        this.skills.add(skill);
     }
 
     public void addEmail(String eMail) {
-
+        this.eMail = eMail;
     }
 
     public void addPhoneNum(String phoneNum) {
-
+        this.phoneNum = phoneNum;
     }
 
-    /*
+    
     public void deleteWorkExperience(WorkExperience workExperience) {
-
+        workExperiences.remove(workExperience);
     }
     
     public void deleteExtraCurricular(Extracurricular extracurricular) {
-
+        extracurriculars.remove(extracurricular);
     }
-
-    */
 
     public void deleteSkill(String skill) {
-
+        skills.remove(skill);
     }
 
-    /*
-    public bool contains(WorkExperience workExperience) {
-
+    public boolean contains(WorkExperience workExperience) {
+        return workExperiences.contains(workExperience);
     }
 
-    public bool contains(Extracurricular extracurricular) {
-
+    public boolean contains(Extracurricular extracurricular) {
+        return extracurriculars.contains(extracurricular);
     }
-
-    */
 
     public String toString() {
-        return "";
+        String ret = "Resume of\n" + lastName + ", " + firstName + "\n"
+        + "E-mail: " + eMail + "\n"
+        + "Phone Number: " + phoneNum + "\n"
+        + "Skills:\n";
+
+        for(int i = 0; i < skills.size(); i++) {
+            ret += skills.get(i) + "\n";
+        }
+
+        // This is assuming workexperience and extracurricular and education get a ToString at some point
+        ret += "Education:\n";
+        for(int i = 0; i < education.size(); i++) {
+            ret += education.get(i) + "\n";
+        }
+    
+        ret += "Work Experiences:\n";
+        for(int i = 0; i < workExperiences.size(); i++) {
+            ret += workExperiences.get(i) + "\n";
+        }
+
+        ret += "Extracurriculars:\n";
+        for(int i = 0; i < extracurriculars.size(); i++) {
+            ret += extracurriculars.get(i) + "\n";
+        }
+
+        return ret;
     }
 }
