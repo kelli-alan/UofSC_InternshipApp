fcd0b2cd91f07ac95260a694fb512918f6b609ea
YousefAfshar <YousefSAfshar@gmail.com>
2021-10-26 14:28:07 -0400

    Begun commenting on User

--- src/User.java
+++ src/User.java
@@ -1,11 +1,13 @@
+//Necessary for declaration of the UUID id in the User class.
 import java.util.UUID;
 
 enum Users {
   STUDENT, EMPLOYER, MODERATOR;
 }
 
-/*
- *
+/**
+ * The User class defines what is contained with a user as well as enumerating
+ * what the three distinct user types are in: student, employer, and moderator.
  * @author Yousef Afshar
  */
 public class User {
@@ -17,7 +19,9 @@ public class User {
   protected String password;
   protected Users type;
 
-  // constructors for loading users who already have an id
+  /*
+   * Constructors for loading user with a pre-existing ID.
+   */
   public User(UUID id, String firstName, String lastName, String username, String password, Users type) {
     this.id = id;
     this.firstName = firstName;
@@ -27,7 +31,9 @@ public class User {
     this.type = type;
   }
 
-  // constructor for creating users who need an id
+  /** 
+   *  Constructor for creating users who need to have to an ID.
+   */
   public User(String firstName, String lastName, String username, String password, Users type) {
     this.id = UUID.randomUUID();
     this.firstName = firstName;
@@ -37,26 +43,50 @@ public class User {
     this.type = type;
   }
 
+  /**
+   * Gets the user's unique UUID ID on their account.
+   * @return this instance of id.
+   */
   public UUID getID() {
     return this.id;
   }
 
+  /**
+   * Gets the user's first name tied to their account.
+   * @return this instance of firstname.
+   */
   public String getFirstName() {
     return this.firstName;
   }
 
+  /**
+   * Gets the user's last name tied to their account.
+   * @return this instance of lastname.
+   */
   public String getLastName() {
     return this.lastName;
   }
 
+  /**
+   * Gets a the user's username tied to their account.
+   * @return this instance of username. 
+   */
   public String getUserName() {
     return this.username;
   }
 
+  /**
+   * Gets the password that users have tied to their account.
+   * @return this instance of a password.
+   */
   public String getPassword() {
     return this.password;
   }
 
+  /**
+   * Gets a type of user (types being STUDENT, EMPLOYER, and MODERATOR)
+   * @return this instance of type. 
+   */
   public Users getType() {
     return this.type;
   }
