952f162115f08d8979ace67ae874fc4fe9860af6
ClarkRM <robbie.0124@gmail.com>
2021-10-28 13:37:30 -0400

    Updating UI to have resume creator

--- src/InternshipUI.java
+++ src/InternshipUI.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.time.Month;
 
 /**
  * 
@@ -201,10 +202,14 @@ public class InternshipUI {
 
         if(sType.equalsIgnoreCase("s")) {
             Users type = Users.STUDENT;
-            Student newStudent = new Student(firstname, lastname, username, password, type);
-            //ADD RESUME CREATOR
-            user = newStudent;
-            app.addUser(newStudent);
+            System.out.print("Enter your email: ");
+            String email = scanner.nextLine();
+            System.out.print("Enter your phone number: ");
+            String phonenum = scanner.nextLine();
+            student = new Student(firstname, lastname, username, password, type);
+            createResume();
+            user = student;
+            app.addUser(student);
         }
         else if(sType.equalsIgnoreCase("e")) {
             Users type = Users.EMPLOYER;
@@ -260,10 +265,246 @@ public class InternshipUI {
         }
     }
 
+
+    private void createResume() {
+
+        Resume res = new Resume();
+
+    clearScreen();
+    String cont = "y";
+
+    while (cont.equalsIgnoreCase("y")) {
+      System.out.println("Enter your skills (enter \"done\" when finished): "); //take out of for loop
+      String skill = scanner.nextLine();
+      if (!skill.equalsIgnoreCase("done"))
+        res.addSkill(skill);
+      else {
+        cont = skill;
+      }
+    }
+
+    clearScreen();
+
+    cont = "y";
+    while (cont.equalsIgnoreCase("y")) {
+
+      System.out.println("Enter the university: ");
+      String university = scanner.nextLine();
+
+      System.out.println("Enter the city: ");
+      String city = scanner.nextLine();
+
+      System.out.println("Enter the state: ");
+      String state = scanner.nextLine();
+
+      System.out.println("Enter the degree type: ");
+      String degreeType = scanner.nextLine();
+
+      System.out.println("Enter your major: ");
+      String major = scanner.nextLine();
+
+      System.out.println("Enter the number of your graduation month (January is 1, February is 2, etc.): ");
+      int monthNumber = scanner.nextInt();
+      scanner.nextLine();
+      Month gradMonth = determineMonth(monthNumber);
+
+      System.out.println("Enter your graduation year: ");
+      int gradYear = scanner.nextInt();
+      scanner.nextLine();
+
+      Education edu = new Education(university, city, state, degreeType, major, gradMonth, gradYear);
+
+      System.out.println("Enter your minor (enter \"none\" if you don't have one): ");
+      String minor = scanner.nextLine();
+      if (!minor.equalsIgnoreCase("none"))
+        edu.addMinor(minor);
+
+      System.out.println("Enter your GPA (Enter -1 if you do not wish to include a GPA): ");
+      double gpa = scanner.nextDouble();
+      scanner.nextLine();
+      if (gpa != -1)
+        edu.addGPA(gpa);
+
+      res.addEducation(edu);
+
+      System.out.println("Would you like to enter another education? (y)es or (n)o?");
+      cont = scanner.nextLine();
+    }
+
+    clearScreen();
+
+    // create work experience section
+
+    System.out.println("Would you like to enter any work experience? (y)es or (n)o?");
+    cont = scanner.nextLine();
+
+    while (cont.equalsIgnoreCase("y")) {
+
+      System.out.println("Enter the position: ");
+      String position = scanner.nextLine();
+
+      System.out.println("Enter the company: ");
+      String company = scanner.nextLine();
+
+      System.out.println("Enter the city: ");
+      String Wcity = scanner.nextLine();
+
+      System.out.println("Enter the state: ");
+      String Wstate = scanner.nextLine();
+
+      System.out.println("Enter the number of the month you started (January is 1, February is 2, etc.): ");
+      int monthNumber = scanner.nextInt();
+      scanner.nextLine();
+      Month startMonth = determineMonth(monthNumber);
+
+      System.out.println("Enter the year you started: ");
+      int startYear = scanner.nextInt();
+      scanner.nextLine();
+
+      /*
+       * creates a work experience with given position, month, startYear, company,
+       * city, state; work experience constructor creates a responsibilities array for
+       * this work experience
+       */
+      WorkExperience workXP = new WorkExperience(position, startMonth, startYear, company, Wcity, Wstate);
+
+      String inner = "no";
+      System.out.println("Enter responsibilities (type \"done\" when finished)");
+      while (!inner.equalsIgnoreCase("done")) {
+        inner = scanner.nextLine();
+        if (!inner.equalsIgnoreCase("done"))
+          workXP.addResponsibility(inner);
+      }
+
+      System.out.println("Are you currently working this position? (y)es or (n)o");
+        String response = scanner.nextLine();
+        if(response.equalsIgnoreCase("n")) {
+            System.out.println("Enter the number of the month you ended (January is 1, February is 2, etc.): ");
+            monthNumber = scanner.nextInt();
+            scanner.nextLine();
+            Month endMonth = determineMonth(monthNumber);
+
+            System.out.println("Enter the year you ended: ");
+            int endYear = scanner.nextInt();
+            scanner.nextLine();
+
+            workXP.addEndDate(endMonth, endYear);
+        }
+      res.addWorkExperience(workXP);
+      System.out.println("Would you like to add another work experience? (y)es or (n)o?");
+      cont = scanner.nextLine();
+    }
+
+    clearScreen();
+
+    System.out.println("Would you like to enter any extracurriculars? (y)es or (n)o?");
+    cont = scanner.nextLine();
+
+    while (cont.equalsIgnoreCase("y")) {
+      System.out.println("Enter the name of the extracurricular organization: ");
+      String title = scanner.nextLine();
+
+      System.out.println("Enter your position within the organization: ");
+      String position = scanner.nextLine();
+
+      System.out.println("Enter the number of the month you joined (January is 1, February is 2, etc.): ");
+      int monthNumber = scanner.nextInt();
+      scanner.nextLine();
+      Month startMonth = determineMonth(monthNumber);
+
+      System.out.println("Enter the year you joined: ");
+      int startYear = scanner.nextInt();
+      scanner.nextLine();
+
+      Extracurricular extrac = new Extracurricular(position, startMonth, startYear, title);
+
+      String inner = "no";
+      System.out.println("Enter responsibilities/activities (type \"done\" when finished)");
+      while (!inner.equalsIgnoreCase("done")) {
+        inner = scanner.nextLine();
+        if (!inner.equalsIgnoreCase("done"))
+          extrac.addExtracurricularActivity(inner);
+      }
+
+      System.out.println("Are you currently in this organization? (y)es or (n)o");
+      if (scanner.nextLine().equalsIgnoreCase("n")) {
+        System.out.println("Enter the number of the month you ended (January is 1, February is 2, etc.): ");
+        monthNumber = scanner.nextInt();
+        scanner.nextLine();
+        Month endMonth = determineMonth(monthNumber);
+
+        System.out.println("Enter the year you ended: ");
+        int endYear = scanner.nextInt();
+        scanner.nextLine();
+        extrac.addEndDate(endMonth, endYear);
+      }
+
+      res.addExtracurricular(extrac);
+
+      System.out.println("Would you like to add another extracurricular? (y)es or (n)o?");
+      cont = scanner.nextLine();
+    }
+    clearScreen();
+
+    student.addResume(res); 
+
+    System.out.println("Resume Created!");
+
+    }
+
+
     //clears the info on the screen
     private void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+      }
+
+      private Month determineMonth(int monthNumber) {
+        Month month = Month.JANUARY;
+        while (monthNumber < 1 || monthNumber > 12) {
+          System.out.println("Invalid Month. Please enter a number 1 through 12");
+          monthNumber = scanner.nextInt();
+          scanner.nextLine();
+        }
+    
+        switch (monthNumber) {
+        case 1:
+          month = Month.JANUARY;
+          break;
+        case 2:
+          month = Month.FEBRUARY;
+          break;
+        case 3:
+          month = Month.MARCH;
+          break;
+        case 4:
+          month = Month.APRIL;
+          break;
+        case 5:
+          month = Month.MAY;
+          break;
+        case 6:
+          month = Month.JUNE;
+          break;
+        case 7:
+          month = Month.JULY;
+          break;
+        case 8:
+          month = Month.AUGUST;
+          break;
+        case 9:
+          month = Month.SEPTEMBER;
+          break;
+        case 10:
+          month = Month.OCTOBER;
+          break;
+        case 11:
+          month = Month.NOVEMBER;
+          break;
+        case 12:
+          month = Month.DECEMBER;
+        }
+        return month;
       }
 
     public static void main(String[] args) {
