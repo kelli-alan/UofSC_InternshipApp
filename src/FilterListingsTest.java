09d74dee32f3b65f840f9c4dfc44b91fe47e9f81
PikHacker <evangrune@gmail.com>
2021-11-14 21:08:08 -0500

    resume test and filterlistings test

---
+++ src/FilterListingsTest.java
@@ -0,0 +1,92 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import java.util.ArrayList;
+
+public class FilterListingsTest {
+
+    private ArrayList<Listing> list;
+
+    @BeforeEach
+    public void setup() {
+        list = new ArrayList<Listing>();
+    }
+
+    @Test
+    public void testFilterEmptyList() {
+        ArrayList<Listing> listnew = FilterListings.filterByHoursPerWeek(list, 0);
+        assertEquals(listnew.size(), 0);
+    }
+
+    @Test
+    public void testFilterHours() {
+        Listing test = new Listing();
+        test.setHoursPerWeek(10);
+        list.add(test);
+
+        test = new Listing();
+        test.setHoursPerWeek(20);
+        list.add(test);
+    
+        test = new Listing();
+        test.setHoursPerWeek(30);
+        list.add(test);
+
+        ArrayList<Listing> listnew = FilterListings.filterByHoursPerWeek(list, 20);
+        assertEquals(listnew.size(), 2);
+    }
+
+    @Test
+    public void testFilterLocation() {
+        Listing test = new Listing();
+        test.setState("SC");
+        test.setCity("Columbia");
+        list.add(test);
+
+        test = new Listing();
+        test.setState("SC");
+        test.setCity("Charleston");
+        list.add(test);
+    
+        test = new Listing();
+        list.add(test);
+
+        ArrayList<Listing> listnew = FilterListings.filterByLocation(list, "Columbia");
+        assertEquals(listnew.size(), 1);
+    }
+
+    @Test
+    public void testFilterSkill() {
+        Listing test = new Listing();
+        test.addSkills("Skill");
+        list.add(test);
+
+        test = new Listing();
+        test.addSkills("Programming");
+        list.add(test);
+    
+        test = new Listing();
+        list.add(test);
+
+        ArrayList<Listing> listnew = FilterListings.filterBySkill(list, "Skill");
+        assertEquals(listnew.size(), 1);
+    }
+
+    @Test
+    public void testFilterPay() {
+        Listing test = new Listing();
+        test.setPay(10.0);
+        list.add(test);
+
+        test = new Listing();
+        test.setPay(20.0);
+        list.add(test);
+    
+        test = new Listing();
+        list.add(test);
+
+        ArrayList<Listing> listnew = FilterListings.filterByPay(list, 10.0);
+        assertEquals(listnew.size(), 2);
+    }
+
+}
---
+++ src/ResumeTest.java
@@ -0,0 +1,89 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Month;
+
+import org.junit.jupiter.api.*;
+
+public class ResumeTest {
+
+    private Resume resume;
+
+    @BeforeEach
+    public void setup() {
+        resume = new Resume();
+    }
+
+    @Test
+    public void testAddNewSkill() {
+        resume.addSkill("Web Design");
+        assertEquals(resume.getSkills().size(), 1);
+    }
+
+    @Test
+    public void testAddWorkExperience() {
+        resume.addWorkExperience(new WorkExperience("position", Month.JUNE, 2021, "company", "city", "state"));
+        assertEquals(resume.getWorkExperiences().size(), 1);
+    }
+
+    @Test
+    public void testAddEducation() {
+        resume.addEducation(new Education("university", "city", "state", "degreeType", "Major", Month.DECEMBER, 2022));
+        assertEquals(resume.getEducations().size(), 1);
+    }
+
+    @Test
+    public void testAddExtraCurricular() {
+        resume.addExtracurricular(new Extracurricular("position", Month.JANUARY, 2020, "title"));
+        assertEquals(resume.getExtracurriculars().size(), 1);
+    }
+
+    @Test
+    public void testRemoveEducation() {
+        resume.addEducation(new Education("university", "city", "state", "degreeType", "Major", Month.DECEMBER, 2022));
+        resume.deleteEducation(0);
+        assertEquals(resume.getEducations().size(), 0);
+    }
+
+    @Test
+    public void testRemoveWorkExperience() {
+        resume.addWorkExperience(new WorkExperience("position", Month.JUNE, 2021, "company", "city", "state"));
+        resume.deleteWorkExperience(0);
+        assertEquals(resume.getWorkExperiences().size(), 0);
+    }
+
+    @Test
+    public void testRemoveSkill() {
+        resume.addSkill("Web Design");
+        resume.deleteSkill(0);
+        assertEquals(resume.getSkills().size(), 0);
+    }
+
+    @Test
+    public void testRemoveExtracurricular() {
+        resume.addExtracurricular(new Extracurricular("position", Month.JANUARY, 2020, "title"));
+        resume.deleteExtraCurricular(0);
+        assertEquals(resume.getExtracurriculars().size(), 0);
+    }
+
+    @Test
+    public void testDisplaySkills() {
+        resume.addSkill("Skill_1");
+        resume.addSkill("Skill_2");
+        assertEquals(resume.displaySkills(), "1: Skill_1\n\n2: Skill_2\n\n");
+    }
+
+    @Test
+    public void testAddDuplicateEducation() {
+        resume.addEducation(new Education("university", "city", "state", "degreeType", "Major", Month.DECEMBER, 2022));
+        resume.addEducation(new Education("university", "city", "state", "degreeType", "Major", Month.DECEMBER, 2022));
+        assertEquals(resume.getEducations().size(), 1);
+    }
+
+    @Test
+    public void testRemoveInvalidSkill() {
+        resume.addSkill("Web Design");
+        resume.deleteSkill(1);
+        assertEquals(resume.getSkills().size(), 1);
+    }
+
+}
