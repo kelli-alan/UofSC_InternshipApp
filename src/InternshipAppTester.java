661971a8fe2b2dc9b0fa90230260b6872c5a47e3
ClarkRM <robbie.0124@gmail.com>
2021-11-14 23:13:42 -0500

    InternshipAppTester name

--- src/InternshipAppTester.java
+++ src/InternshipAppTester.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author Robbie Clark
+ */
 public class InternshipAppTester {
     
     private InternshipApp internshipApp = new InternshipApp();
